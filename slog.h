060bcf44193e605b1f7d803888f484c3a213b889
IvayloTsankov <ivaylotsankov93@gmail.com>
2016-02-07 01:59:51 +0200

    * Fix everything and add tests

--- slog.h
+++ slog.h
@@ -1,9 +1,6 @@
 #ifndef SLOG_H_
 #define SLOG_H_
 
-#include <chrono>
-#include <cstdint>
-
 // Installing:
 //      When compile add -DSLOGGING or/and -DLOGGING_DEBUG
 // Usage:
@@ -14,18 +11,40 @@
 // Change outputfile:
 //      SLOG_set_output(FILE* output);
 
-#include <string>
-
-static int64_t GetUTCInMilliseconds()
-{
-    return std::chrono::duration_cast<std::chrono::milliseconds>
-           (std::chrono::high_resolution_clock::now().time_since_epoch()).count();
-}
-
 // Simple logging mechanism
 #if defined (SLOGGING)
+#   include <string>
+#   include <chrono>
+#   include <cstdint>
+#   include <cstdio>
+
+#if defined (WIN_32)
+#   include "WinBase.h"
+#   define DWORD pid_t;
+#else
+#   include <unistd.h>
+#endif // (WIN_32)
+
+    static pid_t SLOG_getpid()
+    {
+#if defined (_WIN32)
+        return GetProcessId();
+#else
+        return getpid();
+#endif // (_WIN32)
+    }
+
+    static long long int SLOG_get_utc_millisec()
+    {
+        return std::chrono::duration_cast<std::chrono::milliseconds>
+               (std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+    }
+
 	static FILE* log_output = stdout;
-	#define SLOG(a, ...) fprintf(log_output, "(ts: %lld)(pid: %d) " a "\n", (long long int)GetUTCInMilliseconds(), ws_getpid(), ##__VA_ARGS__);
+	#define SLOG(a, ...) fprintf(log_output, "(ts: %lld)(pid: %d) " a "\n", \
+                                 SLOG_get_utc_millisec(), \
+                                 SLOG_getpid(), ##__VA_ARGS__);
+
     static void SLOG_set_output(FILE* output)
     {
          log_output = output;
---
+++ test/test.cpp
@@ -0,0 +1,9 @@
+#include "../slog.h"
+
+
+int main()
+{
+    SLOG("Hello slog test");
+    SLOG_DEBUG("Hello debug slog test");
+    return (0);
+}
